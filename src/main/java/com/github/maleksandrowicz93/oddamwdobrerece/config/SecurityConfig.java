133fd56002e25c9ee9945da03c5930f418d2efc3
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2019-05-06 14:03:01 +0200

    add app basic configuration and project structure

---
+++ src/main/java/com/github/maleksandrowicz93/oddamwdobrerece/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.github.maleksandrowicz93.oddamwdobrerece.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("pass")).roles("ADMIN");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("SELECT username, password, true FROM username = ?")
+                .authoritiesByUsernameQuery("SELECT username, 'ROLE_USER' FROM users WHERE username = ?");
+    }
+
+    @Override
+    protected void configure (HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/register").permitAll()
+                .antMatchers("/login").anonymous()
+                .antMatchers("/app/**").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/app", true)
+                .and()
+            .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout")
+                .and()
+            .csrf()
+                .disable()
+            .rememberMe()
+                .key("keyRememberMe")
+                .rememberMeParameter("remember-me")
+                .tokenValiditySeconds(7*24*60*60);
+        super.configure(http);
+    }
+
+}
