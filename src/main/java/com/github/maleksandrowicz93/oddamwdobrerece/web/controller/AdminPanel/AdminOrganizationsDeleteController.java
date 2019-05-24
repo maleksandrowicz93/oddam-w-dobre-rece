acd6ed49019711e83b87c97879b71653e8f3e011
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2019-05-24 17:41:42 +0200

    add organization delete page

---
+++ src/main/java/com/github/maleksandrowicz93/oddamwdobrerece/web/controller/AdminPanel/AdminOrganizationsDeleteController.java
@@ -0,0 +1,35 @@
+package com.github.maleksandrowicz93.oddamwdobrerece.web.controller.AdminPanel;
+
+import com.github.maleksandrowicz93.oddamwdobrerece.domain.model.Organization;
+import com.github.maleksandrowicz93.oddamwdobrerece.domain.model.User;
+import com.github.maleksandrowicz93.oddamwdobrerece.domain.repositories.OrganizationRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/organizations/delete")
+public class AdminOrganizationsDeleteController {
+
+    private OrganizationRepository organizationRepository;
+
+    public AdminOrganizationsDeleteController(OrganizationRepository organizationRepository) {
+        this.organizationRepository = organizationRepository;
+    }
+
+    @GetMapping("/{id}")
+    public String displayDeleteOrganizationPage(@PathVariable("id") Long id, Model model) {
+        Organization organization = organizationRepository.findFirstById(id);
+        model.addAttribute("organization", organization);
+        return "admin-organizations-delete";
+    }
+
+    @GetMapping("/{id}/confirm")
+    public String confirmDeleteOrganization(@PathVariable("id") Long id) {
+        organizationRepository.deleteById(id);
+        return "redirect:/admin/organizations";
+    }
+
+}
