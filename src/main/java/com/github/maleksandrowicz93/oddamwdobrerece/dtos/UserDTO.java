b53568452f7b82a6634cf3f1c5f4f0190e8aecf4
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2019-05-31 08:36:43 +0200

    add register method add email validation

--- src/main/java/com/github/maleksandrowicz93/oddamwdobrerece/dtos/UserDTO.java
+++ src/main/java/com/github/maleksandrowicz93/oddamwdobrerece/dtos/UserDTO.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+
 @Data @NoArgsConstructor @AllArgsConstructor
 public class UserDTO {
 
+    @Email
     private String username;
     private String password;
     private String confirmedPassword;
