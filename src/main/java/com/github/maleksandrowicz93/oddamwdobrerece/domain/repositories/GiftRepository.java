c8e597ad9a7528b705d597d29d9e49e5410ede31
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2019-08-24 18:11:38 +0200

    correct method returning and sorting all user gifts

--- src/main/java/com/github/maleksandrowicz93/oddamwdobrerece/domain/repositories/GiftRepository.java
+++ src/main/java/com/github/maleksandrowicz93/oddamwdobrerece/domain/repositories/GiftRepository.java
@@ -1,8 +1,10 @@
 package com.github.maleksandrowicz93.oddamwdobrerece.domain.repositories;
 
 import com.github.maleksandrowicz93.oddamwdobrerece.domain.model.Gift;
+import com.github.maleksandrowicz93.oddamwdobrerece.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,7 +12,8 @@ public interface GiftRepository extends JpaRepository<Gift, Long> {
 
     Gift findFirstById(Long id);
 
-    @Query("select g from Gift g order by g.status desc, g.dateOfCollection desc, g.dateOfGiftRegistration")
-    List<Gift> readAllSortedGifts();
+    @Query("select g from Gift g where g.user = :user " +
+            "order by g.status desc, g.dateOfCollection desc, g.dateOfGiftRegistration desc, g.id desc ")
+    List<Gift> readAllSortedGiftsOfGivenUser(@Param("user") User user);
 
 }
