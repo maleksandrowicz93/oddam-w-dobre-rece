b863be5937c69558d1720f74c2f69fbeeedefcd3
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2019-08-26 19:02:37 +0200

    #10 add processing of edit user profile form, add method returning user

--- src/main/java/com/github/maleksandrowicz93/oddamwdobrerece/web/controller/UserPanel/UserEditProfileController.java
+++ src/main/java/com/github/maleksandrowicz93/oddamwdobrerece/web/controller/UserPanel/UserEditProfileController.java
@@ -7,10 +7,12 @@ import com.github.maleksandrowicz93.oddamwdobrerece.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
 
-@Controller
+@Controller("/app/profile")
 public class UserEditProfileController {
 
     private UserService userService;
@@ -21,10 +23,22 @@ public class UserEditProfileController {
 
     @GetMapping
     public String displayEditProfileForm(Principal principal, Model model) {
-        User user = userService.findByUsername(principal.getName());
+        User user = getUser(principal);
         UserDTO userDTO = UserConverter.userToUserDto(user);
         model.addAttribute("userDTO", userDTO);
         return "user-editProfile";
+    }
+
+    @PostMapping
+    public String confirmEditProfileForm(@ModelAttribute UserDTO userDTO, Principal principal) {
+        User user = getUser(principal);
+        UserConverter.saveUserChanges(userDTO, user);
+        userService.saveUser(user);
+        return "redirect:/app/profile/success";
+    }
+
+    private User getUser(Principal principal) {
+        return userService.findByUsername(principal.getName());
     }
 
 }
