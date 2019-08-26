c6d75e8540e9806bef4cbe8437cf1f12eb54db72
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2019-08-26 20:15:30 +0200

    #10 add edit profile form validation

--- src/main/java/com/github/maleksandrowicz93/oddamwdobrerece/web/controller/UserPanel/UserEditProfileController.java
+++ src/main/java/com/github/maleksandrowicz93/oddamwdobrerece/web/controller/UserPanel/UserEditProfileController.java
@@ -6,6 +6,7 @@ import com.github.maleksandrowicz93.oddamwdobrerece.services.UserConverter;
 import com.github.maleksandrowicz93.oddamwdobrerece.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,8 +31,21 @@ public class UserEditProfileController {
     }
 
     @PostMapping
-    public String confirmEditProfileForm(@ModelAttribute UserDTO userDTO, Principal principal) {
+    public String confirmEditProfileForm(@ModelAttribute UserDTO userDTO, Principal principal, BindingResult result) {
         User user = getUser(principal);
+        if (result.hasErrors()) {
+            return "redirect:/app/profile";
+        }
+        if(!user.getUsername().equals(userDTO.getUsername()) || !user.getPassword().equals(userDTO.getPassword())) {
+            if (!userService.checkPasswordEquality(userDTO)) {
+                result.rejectValue("password", null, "Hasło i powtórzone hasło są niezgodne");
+                return "redirect:/app/profile";
+            }
+            if (!userService.checkIsUserNameAvailable(userDTO)) {
+                result.rejectValue("username", null, "Nazwa użytkownika jest już zajęta");
+                return "redirect:/app/profile";
+            }
+        }
         UserConverter.saveUserChanges(userDTO, user);
         userService.saveUser(user);
         return "redirect:/app/profile/success";
