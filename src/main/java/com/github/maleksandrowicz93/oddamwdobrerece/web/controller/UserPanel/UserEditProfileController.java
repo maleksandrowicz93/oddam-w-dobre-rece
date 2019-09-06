9eb5a45eff9544fc01bd1375e0ba6803bba77b6f
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2019-09-06 16:44:08 +0200

    #10 add logic allowing getting logged user data after change of user email into UserEditProfileController

--- src/main/java/com/github/maleksandrowicz93/oddamwdobrerece/web/controller/UserPanel/UserEditProfileController.java
+++ src/main/java/com/github/maleksandrowicz93/oddamwdobrerece/web/controller/UserPanel/UserEditProfileController.java
@@ -19,6 +19,8 @@ import java.security.Principal;
 public class UserEditProfileController {
 
     private UserService userService;
+    private boolean isUserWasUpdated = false;
+    private Long userID;
 
     public UserEditProfileController(UserService userService) {
         this.userService = userService;
@@ -27,6 +29,11 @@ public class UserEditProfileController {
     @GetMapping
     public String displayEditProfileForm(Principal principal, Model model) {
         User user = getUser(principal);
+        if (isUserWasUpdated) {
+            if (user == null) {
+                user = userService.findById(userID);
+            }
+        }
         UserDTO userDTO = UserConverter.userToUserDto(user);
         model.addAttribute("userDTO", userDTO);
         return "user-editProfile";
@@ -38,7 +45,7 @@ public class UserEditProfileController {
         if (result.hasErrors()) {
             return "redirect:/app/profile";
         }
-        if(!user.getUsername().equals(userDTO.getUsername()) || !user.getPassword().equals(userDTO.getPassword())) {
+        if (!userDTO.getPassword().equals("") || !userDTO.getUsername().equals(user.getUsername())) {
             if (!userService.checkPasswordEquality(userDTO)) {
                 result.rejectValue("password", null, "Hasło i powtórzone hasło są niezgodne");
                 return "redirect:/app/profile";
@@ -48,8 +55,12 @@ public class UserEditProfileController {
                 return "redirect:/app/profile";
             }
         }
-        UserConverter.saveUserChanges(userDTO, user);
+        userService.saveUserChanges(userDTO, user);
         userService.saveUser(user);
+        if (!isUserWasUpdated) {
+            userID = user.getId();
+        }
+        isUserWasUpdated = true;
         return "redirect:/app/profile/success";
     }
 
